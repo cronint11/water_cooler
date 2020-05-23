35d9f2a3a695e042779fec1f470506c6a8860ddf
Six4 <tm.timcronin@gmail.com>
2020-05-23 13:44:07 -0400

    login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Login</h4>
+      <span>handle:</span><input placeholder="username"></input>
+      <span>password:</span><input type="password" placeholder="password"></input>
     </div>
   );
 }
