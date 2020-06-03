92f0c1be9f7278075ca476202bf130967ef8afe5
lweller1203 <58568467+lweller1203@users.noreply.github.com>
2020-06-03 18:39:18 -0400

    Create seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (handle, pwd, email) VALUES ("ADMIN", "password", "admin@admin.com");
+
+INSERT INTO users (handle, pwd, email) VALUES ("ADMIN2", "password2", "admin2@admin.com");
+
+INSERT INTO friends (user_id_1, user_id_2) VALUES ("1", "2")
+
+INSERT INTO subscriptions (user_id, subscription_id) VALUES (1, 2)
+
+
+
+
+
+
+
+
+
+
