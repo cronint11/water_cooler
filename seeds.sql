5d45ac048d5acf4f33b1f740cc80ad49334098bf
lweller1203 <58568467+lweller1203@users.noreply.github.com>
2020-06-03 18:41:30 -0400

    Update seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -6,6 +6,8 @@ INSERT INTO friends (user_id_1, user_id_2) VALUES ("1", "2")
 
 INSERT INTO subscriptions (user_id, subscription_id) VALUES (1, 2)
 
+INSERT INTO subscription (name, type_id, subscription_id) VALUES ("ADMIN", "1", "1")
+
 
 
 
