98cf88f8c4bc5a6ca78809a1412af66d8f2e7965
Six4 <tm.timcronin@gmail.com>
2020-06-15 03:59:32 -0400

    Back to dummy code for the back end.

--- src/components/Subscriptions.js
+++ src/components/Subscriptions.js
@@ -6,13 +6,21 @@ class Subscriptions extends React.Component {
   constructor(props)
   {
     super(props);
+    this.state = {
+      subscriptions: subs[props.location.state.media],
+      mediaType: props.location.state.media
+    };
+    console.log(this.state);
   }
 
 
   render() {
     return (
       <span>
-        {subs[this.props.media].map()}
+        possible subscriptions for {this.state.mediaType}
+        <ul>
+        {this.state.subscriptions.map((item, index) => <li>{item.service}</li>)}
+        </ul>
       </span>
     );
   }
