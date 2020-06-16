4657b5bacec6a02d884ec77686eeed9045a68404
Jose Medina <yanoya10@hotmail.com>
2020-06-15 21:32:48 -0400

    styling changes to header and other components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Navbar, Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 function Footer() {
     return (   
-    <Container className='sticky-footer'>
-    <h6 className="slogan">We know movies, music, tv shows, books, podcasts, video games and more...</h6>      
-    <Navbar  sticky="bottom" className="bottom">
-    <Navbar.Brand id="footer" href="/feed">Home</Navbar.Brand>
-    <Navbar.Brand id="footer" href="/media">Profile</Navbar.Brand>
-    <Navbar.Brand id="footer" href="/watchlist">Watchlist</Navbar.Brand>
-    <Navbar.Brand id="footer" href="/friends">Friends</Navbar.Brand>
-    </Navbar>
-    
-    </Container>
-    )}
+        <div className="sticky-footer">
+            <h6 className="slogan">We know movies, music, tv shows, books, podcasts, video games and more...</h6>      
+            <Link id="footer" to="/feed">Home</Link>
+            <Link id="footer" to="/media">Profile</Link>
+            <Link id="footer" to="/watchlist">Watchlist</Link>
+            <Link id="footer" to="#">Friends</Link>
+        </div>
+    )
+}
 
 export default Footer;
