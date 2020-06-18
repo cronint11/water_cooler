4db4e32f08210e73e88038fcdbcb12a6137c3681
Six4 <tm.timcronin@gmail.com>
2020-06-17 22:18:23 -0400

    Updates

---
+++ precache-manifest.1a06e1ebabbac2b4b94975861c06115f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8d763659dd13bcbb028fea37017d02",
+    "url": "/water_cooler/index.html"
+  },
+  {
+    "revision": "9e7ca9bf4bee68284f6a",
+    "url": "/water_cooler/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "87bdbe44a5c1bd32607f",
+    "url": "/water_cooler/static/css/main.fb04f272.chunk.css"
+  },
+  {
+    "revision": "9e7ca9bf4bee68284f6a",
+    "url": "/water_cooler/static/js/2.231ad48a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/water_cooler/static/js/2.231ad48a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87bdbe44a5c1bd32607f",
+    "url": "/water_cooler/static/js/main.2e61a6b1.chunk.js"
+  },
+  {
+    "revision": "1d5d7af501d708aae591",
+    "url": "/water_cooler/static/js/runtime-main.bcb95b6a.js"
+  },
+  {
+    "revision": "67a56fc5fa8ab3f14363b1e09b8497ad",
+    "url": "/water_cooler/static/media/gold-circle.67a56fc5.png"
+  },
+  {
+    "revision": "2e5dc1811fcf425be02a1bc57db4dd79",
+    "url": "/water_cooler/static/media/water-cooler.2e5dc181.png"
+  },
+  {
+    "revision": "95e5324cb32505a01e9a5cfbe526547f",
+    "url": "/water_cooler/static/media/watercooler-friend.95e5324c.jpg"
+  }
+]);
